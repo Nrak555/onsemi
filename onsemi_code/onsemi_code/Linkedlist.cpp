9b1054e133d7cb9640160ca59fcc38103145d9bd
Jeffrey Jones <Nrak0120@gmail.com>
2018-07-09 04:59:58 -0700

    Finish Linkedlist

--- onsemi_code/onsemi_code/Linkedlist.cpp
+++ onsemi_code/onsemi_code/Linkedlist.cpp
@@ -67,13 +67,34 @@ template <class T> class list
 			tail = nullptr;
 
 		}
+
+		list<T>(node<T>* Node) {	// Takes a slice and converts it into a list.
+			int i = 1;				//Should be garanteed one node
+			node<T>* n = Node;
+			if (n != nullptr) {	// check that they gave at least one node else defualt
+				head = n;
+
+				while (n->get_next()) {
+					n = n->get_next();
+					i++;
+				}
+				size = i;
+				tail = n;
+			}
+			else {
+				size = 0;
+				head = nullptr;
+				tail = nullptr;
+			}
+		}
+
 		~list() {
 
 		}
 		void push(T data) {
 			node<T> *n_ptr = new node<T>(data);
 
-			if (tail == nullptr && head == nullptr) {	//list is empty 
+			if ((tail == nullptr) && (head == nullptr)) {	//list is empty 
 				head = n_ptr;
 				tail = n_ptr;
 			}
@@ -129,6 +150,7 @@ template <class T> class list
 				size++;
 			}
 		}
+
 		int search(T d) {
 			node<T>* n = head;
 			for (int i = 0; i < size; i++) {
@@ -138,13 +160,14 @@ template <class T> class list
 			}
 			return -1;						//did not find item
 		}
-		node* slice(int num1, int num2) {
+
+		node<T>* slice(int num1, int num2) {
 			int j = num1;
 			int k = num2;
 			node<T>* NodeS = head;	// Start node and End node
 			node<T>* NodeE;
 
-			if (num2 < num1) {					
+			if ((num2 < num1) || (num1 > size)) {					
 				std::cout << "Invaild slice";
 				return nullptr;
 			}
@@ -153,12 +176,12 @@ template <class T> class list
 				k = size;
 
 			for (int i = 0; i < j; i++)		//Find Starting node
-				NodeS = NodeS->get_next;
+				NodeS = NodeS->get_next();
 
 			NodeE = NodeS;				// NodeE is after NodeS
 
 			for (int i = j; i < k; i++)	//Find Ending Node
-				NodeE = NodeE->get_next;
+				NodeE = NodeE->get_next();
 
 			if(NodeS->get_last() != nullptr)	//Check if slice includes the beginning
 				NodeS->get_last()->set_next(NodeE->get_next());
@@ -174,13 +197,124 @@ template <class T> class list
 			NodeS->set_last(nullptr);		//isolate the slice
 			NodeE->set_next(nullptr);
 
-			size = size - (k - j);
+			size = size - (k - j + 1);
 
 			return NodeS;
 
 		}
-		void mergesort() {}
-		void reverse_mergesort() {}
+		void mergesort_start() {
+			node<T>* temp;
+			head = mergesort(head);
+			temp = head;
+
+			for (int i = 0; i < size; i++) {
+				std::cout << "\n" << i << ":" << size;
+				temp = temp->get_next();
+			}
+			tail = temp;
+		}
+		void reverse_mergesort_start() {
+			node<T>* temp;
+			head = Reverse_mergesort(head);
+			temp = head;
+
+			for (int i = 0; i < size; i++) {
+				std::cout << "\n" << i << ":" << size;
+				temp = temp->get_next();
+			}
+			tail = temp;
+		}
+
+		node<T>* mergesort(node<T>* h) {
+			node<T>* first;
+			node<T>*second;
+
+			if ((h == nullptr) || (h->get_next() == nullptr))  // check for existince and trival case
+				return h;
+
+			second = split(h);
+
+			first = mergesort(h);
+			second = mergesort(second);
+
+			return merge(h, second);
+		}
+
+		node<T>* Reverse_mergesort(node<T>* h) {
+			node<T>* first;
+			node<T>*second;
+
+			if ((h == nullptr) || (h->get_next() == nullptr))  // check for existince and trival case
+				return h;
+
+			second = split(h);
+
+			first = Reverse_mergesort(h);
+			second = Reverse_mergesort(second);
+
+			return reverse_merge(h, second);
+		}
+		
+
+		node<T>* merge(node<T>* first, node<T>* second) {
+			node<T>* ptr = first;
+
+			if (first == nullptr)
+				return second;
+
+			if (second == nullptr)
+				return first;
+
+			if (first->get_data() < second->get_data()) {
+				first->set_next(merge(first->get_next(), second));
+				first->get_next()->set_last(first);
+				first->set_last(nullptr);
+				return first;
+			}
+			else {
+				second->set_next(merge(first, second->get_next()));
+				second->get_next()->set_last(second);
+				second->set_last(nullptr);
+				return second;
+			}
+		}
+
+		node<T>* reverse_merge(node<T>* first, node<T>* second) {
+			node<T>* ptr = first;
+
+			if (first == nullptr)
+				return second;
+
+			if (second == nullptr)
+				return first;
+
+			if (first->get_data() > second->get_data()) {
+				first->set_next(reverse_merge(first->get_next(), second));
+				first->get_next()->set_last(first);
+				first->set_last(nullptr);
+				return first;
+			}
+			else {
+				second->set_next(reverse_merge(first, second->get_next()));
+				second->get_next()->set_last(second);
+				second->set_last(nullptr);
+				return second;
+			}
+		}
+
+		node<T>* split(node<T>* n) {	// split list into two half length bits
+			node<T>* fast = n;
+			node<T>* slow = n;
+			node<T>* temp;
+
+			while ((fast->get_next() == nullptr)  && (fast->get_next()->get_next() == nullptr)) {
+				fast = fast->get_next()->get_next();
+				slow = slow->get_next();
+			}
+			temp = slow->get_next();
+			slow->set_next(nullptr);
+			return temp;
+		}
 
 		int get_size() {
 			return size;
@@ -208,7 +342,9 @@ template <class T> class list
 
 int main() {
 	int temp;
+	//node<int>* n;
 	list<int> *l = new list<int>();
+	list<int> *l2;
 	
 	l->push(5);
 	l->push(3);
@@ -233,7 +369,21 @@ int main() {
 	std::cout << l->search(11);
 
 	std::cout << *l;
-	std::cout << "\n" << l->get_size();
+	std::cout << "\n size:" << l->get_size();
+
+	l2 = new list<int>( l->slice(3, 5));
+
+	std::cout << "\n size:" << l->get_size();
+	std::cout << "\n size:" << l2->get_size();
+	std::cout << *l;
+	std::cout << *l2;
+	
+	l->mergesort_start();
+	std::cout << *l;
+
+	l->reverse_mergesort_start();
+	std::cout << *l;
+
 	std::cin >> temp;
 
 	return 0;
